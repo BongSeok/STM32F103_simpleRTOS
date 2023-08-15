61bc9fdece182493445f7b29f2c540c922b578ee
BongSeok <lilole@empas.com>
2023-08-15 11:11:27 +0900

    교재 버전

---
+++ OS/kernel/Src/kernel.c
@@ -0,0 +1,16 @@
+#include "stdint.h"
+#include "stdbool.h"
+
+#include "kernel.h"
+#include "task.h"
+
+void Kernel_start(void)
+{
+	Kernel_task_start();
+}
+
+void Kernel_yield(void)
+{
+	Kernel_task_scheduler();
+}
+
